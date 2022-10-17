838ee502fefba8226c20aa58f06340aa4ad70d89
Emad Khan <emadkhanqai@gmail.com>
2022-10-18 02:24:17 +0500

    Initial code working on simple socketio message.

---
+++ index.js
@@ -0,0 +1,32 @@
+const cors=require('cors');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('getUnitStatus', (msg) => {
+        io.emit('receiveMessage', `server: ${msg}`);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
